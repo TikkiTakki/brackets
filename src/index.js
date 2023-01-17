0b87b7b60ef44200b4a98283793e6627544a19c2
TikkiTakki <115143189+TikkiTakki@users.noreply.github.com>
2023-01-17 10:14:30 +0300

    init:add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let openBracets = [];
+  let closeBracets = [];
+  let indexClose;
+  let indexOpen;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      openBracets.push(bracketsConfig[i][0]);
+      closeBracets.push(bracketsConfig[i][1]);
+  }
+  for (let i = 0; i < str.length; i++) {
+      indexOpen = openBracets.indexOf(str[i]);
+      indexClose = closeBracets.indexOf(str[i]);
+      if (stack.length === 0) {
+          stack.push(str[i]);
+          continue;
+      }
+      if (indexClose !== -1) {
+          if (openBracets.indexOf(stack[stack.length - 1]) === indexClose) {
+              stack.pop();
+              continue;
+          }
+      }
+      if (indexOpen !== -1) {
+          stack.push(str[i]);
+          continue;
+      }
+  }
+  if (stack.length !== 0) {
+      return false;
+  }
+  return true;
 }
